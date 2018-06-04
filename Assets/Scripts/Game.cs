62ba6b582d8d271f1eb6eac5de75a6cf24ea1e42
Nguyen Chi Thien <chithien.nguyen@gmail.com>
2018-06-04 23:53:26 +0700

    Render map in game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public Camera gameCamera;
+
+    private float minCamY;
+    private float maxCamY;
+    private float camY;
+        
     private Map map;
 
 	void Start ()
     {
         map = new Map();
         map.Load(1);
-	}
+
+        float screenRatio = (float)Screen.width / Screen.height;
+
+        float viewWidth = map.GetRealWidth();
+        float viewHeight = viewWidth / screenRatio;
+
+        gameCamera.orthographicSize = minCamY = viewHeight / 2.0f;
+        maxCamY = map.GetRealHeight() - minCamY;
+
+        Vector3 camPos = gameCamera.transform.localPosition;
+        camPos.x = viewWidth / 2.0f;
+        camPos.y = camY = minCamY;
+        gameCamera.transform.localPosition = camPos;
+    }
+
+    private void MoveCamera(float dist)
+    {
+        camY = Mathf.Clamp(camY + dist, minCamY, maxCamY);
+        Vector3 camPos = gameCamera.transform.localPosition;
+        camPos.y = camY;
+        gameCamera.transform.localPosition = camPos;
+    }
 	
 	void Update ()
     {
-		
+        float dt = Time.deltaTime;
+        float move = Input.GetAxis("Vertical") * Constants.MOVE_SPEED * dt;
+        MoveCamera(move);
 	}
 }
--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,20 +7,65 @@ using System.Collections.ObjectModel;
 
 public class Map
 {
-    private Sprite[] sprites;
+    private Dictionary<int, Sprite> spriteMap;
 
     private int width;
     private int height;
 
-    private int[,] tileIds;
+    private int tileWidth;
+    private int tileHeight;
+
+    private float tileRealWidth;
+    private float tileRealHeight;
+
+    private SpriteRenderer[,] cellRenderers;
 
     private string basePath = string.Empty;
+
+    public int GetNumCellX()
+    {
+        return width;
+    }
+
+    public int GetNumCellY()
+    {
+        return height;
+    }
+
+    public int GetWidth()
+    {
+        return width * tileWidth;
+    }
+
+    public int GetHeight()
+    {
+        return height * tileHeight;
+    }
+
+    public float GetRealWidth()
+    {
+        return width * tileRealWidth;
+    }
+
+    public float GetRealHeight()
+    {
+        return height * tileRealHeight;
+    }
 
     private Stream GetStream(string name)
     {
         string path = Utils.PathSimplify(basePath + "/" + name);
         TextAsset textAsset = Resources.Load<TextAsset>(path);
         return new MemoryStream(textAsset.bytes);
+    }
+
+    private SpriteRenderer CreateRenderer(float x, float y)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.localPosition = new Vector3(x, y, 0.0f);
+        SpriteRenderer renderer = obj.AddComponent<SpriteRenderer>();
+
+        return renderer;
     }
 
     public void Load(int levelId)
@@ -31,7 +76,35 @@ public class Map
         width = map.Width;
         height = map.Height;
 
-        tileIds = new int[height, width];
+        cellRenderers = new SpriteRenderer[height, width];
+
+        TmxTileset tileset = map.Tilesets[0];
+
+        int tileSetWidth = tileset.Columns.GetValueOrDefault();
+        int tileSetHeight = tileset.TileCount.GetValueOrDefault() / tileSetWidth;
+
+        spriteMap = new Dictionary<int, Sprite>();
+
+        tileWidth = tileset.TileWidth;
+        tileHeight = tileset.TileHeight;
+
+        int startId = tileset.FirstGid;
+
+        Sprite[] spriteList = Resources.LoadAll<Sprite>(Utils.PathSimplify(basePath + "/" + tileset.Name));
+        float pixelsPerUnit = spriteList[0].pixelsPerUnit;
+
+        tileRealWidth = tileWidth / pixelsPerUnit;
+        tileRealHeight = tileHeight / pixelsPerUnit;
+
+        foreach (Sprite sprite in spriteList)
+        {
+            Vector2 center = sprite.rect.center;
+            int column = Mathf.FloorToInt(center.x / tileWidth);
+            int row = tileSetHeight - Mathf.FloorToInt(center.y / tileHeight) - 1;
+            int id = row * tileSetWidth + column;
+
+            spriteMap.Add(id, sprite);
+        }
 
         TmxLayer backgroundLayer = map.Layers[0];
         Collection<TmxLayerTile> tiles = backgroundLayer.Tiles;
@@ -39,13 +112,21 @@ public class Map
 
         for (int i = 0; i < numTiles; ++i)
         {
-            int id = tiles[i].Gid;
+            int id = tiles[i].Gid - startId;
 
-            int row = i / width;
+            int row = height - (i / width) - 1;
             int col = i % width;
-            tileIds[row, col] = id;
 
-            Debug.Log(id);
+            SpriteRenderer cellRenderer = null;
+
+            Sprite sprite;
+            if (spriteMap.TryGetValue(id, out sprite))
+            {
+                cellRenderer = CreateRenderer((col + 0.5f) * tileRealWidth, (row + 0.5f) * tileRealHeight);
+                cellRenderer.sprite = sprite;
+            }
+
+            cellRenderers[row, col] = cellRenderer;
         }
     }
 }
