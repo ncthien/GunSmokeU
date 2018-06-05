c8b8b0a498208056936ec91e09f091e07bf5ee69
Nguyen Chi Thien <chithien.nguyen@gmail.com>
2018-06-05 23:31:52 +0700

    Add basic movement control for characters

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Vector2 position;
+    public float height;
+    public float width;
+
+    private Transform cachedTransform;
+
+    private void Awake()
+    {
+        cachedTransform = transform;
+    }
+
+    public void Move(Vector2 move)
+    {
+        move = Game.Instance.GetMove(position, width, height, move);
+        SetPosition(position + move);
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        this.position = position;
+        cachedTransform.localPosition = position;
+    }
+
+    public Vector2 GetPosition()
+    {
+        return position;
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,44 +5,82 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public Camera gameCamera;
+    public Player player;
 
     private float minCamY;
     private float maxCamY;
     private float camY;
-        
+
+    private float mapWidth;
+    private float mapHeight;
+
+    private float viewWidth;
+    private float viewHeight;
+
     private Map map;
 
-	void Start ()
+    public static Game Instance = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start ()
     {
         map = new Map();
         map.Load(1);
 
         float screenRatio = (float)Screen.width / Screen.height;
 
-        float viewWidth = map.GetRealWidth();
-        float viewHeight = viewWidth / screenRatio;
+        mapWidth = map.GetRealWidth();
+        mapHeight = map.GetRealHeight();
+
+        viewWidth = mapWidth;
+        viewHeight = viewWidth / screenRatio;
 
         gameCamera.orthographicSize = minCamY = viewHeight / 2.0f;
-        maxCamY = map.GetRealHeight() - minCamY;
+        maxCamY = mapHeight - minCamY;
 
         Vector3 camPos = gameCamera.transform.localPosition;
         camPos.x = viewWidth / 2.0f;
         camPos.y = camY = minCamY;
         gameCamera.transform.localPosition = camPos;
+
+        player.SetPosition(new Vector2(camPos.x, 0.0f));
     }
 
-    private void MoveCamera(float dist)
+    private void MoveCameraY(float dist)
     {
-        camY = Mathf.Clamp(camY + dist, minCamY, maxCamY);
+        SetCameraY(camY + dist);
+    }
+
+    private void SetCameraY(float y)
+    {
+        camY = Mathf.Clamp(y, minCamY, maxCamY);
         Vector3 camPos = gameCamera.transform.localPosition;
         camPos.y = camY;
         gameCamera.transform.localPosition = camPos;
     }
-	
-	void Update ()
+
+    public Vector2 GetMove(Vector2 position, float width, float height, Vector2 move)
+    {
+        if (move.x > 0.0f) move.x = Mathf.Min(move.x, mapWidth - position.x - width / 2.0f);
+        else move.x = Mathf.Max(move.x, width / 2.0f - position.x);
+
+        if (move.y > 0.0f) move.y = Mathf.Min(move.y, mapHeight - position.y - height);
+        else move.y = Mathf.Max(move.y, -position.y);
+
+        return move;
+    }
+
+    void Update ()
     {
         float dt = Time.deltaTime;
-        float move = Input.GetAxis("Vertical") * Constants.MOVE_SPEED * dt;
-        MoveCamera(move);
+
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Constants.MOVE_SPEED * dt;
+        player.Move(move);
+        
+        SetCameraY(player.GetPosition().y + viewHeight / 2.0f);
 	}
 }
