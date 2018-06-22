1f4d1b1ef3ca20ff310b60152dd8c1c613b3c2d2
Nguyen Chi Thien <chithien.nguyen@gmail.com>
2018-06-22 07:27:57 +0700

    Add object layers for level 1 map

---
+++ Data/Levels/items.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="items" tilewidth="16" tileheight="24" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <tile id="0">
+  <image width="16" height="16" source="Barrel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="15" height="16" source="Boots.png"/>
+ </tile>
+ <tile id="2">
+  <image width="15" height="16" source="MoneyBag.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="15" source="POW.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="14" source="Rifle.png"/>
+ </tile>
+ <tile id="5">
+  <image width="14" height="15" source="Wanted Poster.png"/>
+ </tile>
+ <tile id="6">
+  <image width="15" height="14" source="Bullets.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="15" source="CowSkull.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="ExtraLife.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="Invincibility.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="23" source="Man.png"/>
+ </tile>
+ <tile id="11">
+  <image width="14" height="24" source="Woman.png"/>
+ </tile>
+</tileset>
